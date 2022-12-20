d6926fa52e232bd079244121d96cc9a1701c3841
AdilZd <nowayat0me@gmail.com>
2022-12-20 09:51:05 +0100

    Fin

---
+++ public/js/main.js
@@ -0,0 +1,60 @@
+// 1 //
+
+let nombre = 8.56;
+console.log (nombre);
+
+// 2 //
+
+console.log(Math.round(nombre));
+
+// 3 //
+
+console.log(Math.ceil(nombre));
+
+// 4 //
+
+console.log(Math.floor(nombre));
+
+// 5 //
+
+console.log(Math.trunc(nombre));
+
+// 6 //
+
+let zeroUn = Math.random();
+console.log(zeroUn)
+
+// 7 // 
+
+let fois100 = Math.random() *100;
+console.log(fois100)
+
+// 8 //
+
+let min = 0; 
+let max = 100;
+let random = Math.floor(Math.random() * (max - min)) + min;
+console.log(random)
+
+// 9 //
+
+let puissance = 8 ** 2;
+console.log(puissance);
+
+// 10 //
+
+let racine = Math.sqrt(9);
+console.log(racine)
+
+// 11 //
+
+let absolue = Math.abs(-1);
+console.log(absolue)
+
+// 12 //
+
+console.log(Math.max(-2, 1000, 8, 57, ));
+
+// 13 //
+
+console.log(Math.min(-2, 1000, 8, 57, ));
